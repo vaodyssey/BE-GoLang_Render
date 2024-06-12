f66f2a2a805f22fc0b48fbe32d596929e6c7ac07
Vzart <vo.dang.khoa.main@gmail.com>
2024-06-12 16:35:06 +0700

    :card_file_box: seed data for products table

---
+++ db/migrations/000003_seed_data.up.sql
@@ -0,0 +1,12 @@
+INSERT INTO products(id, name, image, description, price, created_at)
+VALUES ('f737a92c-d0ea-4c6a-8365-c4714323262d', 'Laptop Asus Vivobook 15X Oled M3504YA-L1268W (AMD Ryzen 5-7530U)', 'https://product.hstatic.net/1000233206/product/laptop-asus-vivobook-15x-oled-m3504ya-l1268w_0a3872590bdf47ea8e4cd7dec4f354d0.png','A nice laptop',14990000,NOW());
+INSERT INTO products(id, name, image, description, price, created_at)
+VALUES ('6ca76017-5818-4b87-9f0f-5d1f5443168b', 'Laptop Gaming Acer Nitro V', 'https://product.hstatic.net/1000233206/product/laptop-gaming-acer-nitro-v-anv15-51-58an_390209814b5e4c1c9a2a70708383234d.png','A nice laptop',18990000,NOW());
+INSERT INTO products(id, name, image, description, price, created_at)
+VALUES ('6825cac4-d8e6-4208-a902-d4533c2023e6 ', 'Laptop Gaming MSI Titan 18', 'https://product.hstatic.net/1000233206/product/laptop-gaming-msi-titan-18-hx-a14vig-205vn_23d36e8836ba432dbd419d622aa76b3c_grande.png','A nice laptop',149990000,NOW());
+INSERT INTO products(id, name, image, description, price, created_at)
+VALUES ('9497d0aa-7df9-42c4-a1ee-d5a2c617708e', 'Laptop Lenovo IdeaPad Slim 3', 'https://product.hstatic.net/1000233206/product/laptop-lenovo-ideapad-slim-3-14iah8-83eq0009vn_f91c28e9d85d4e6189bfac3a8cf3aaa0_grande.png','A nice laptop',16290000,NOW());
+INSERT INTO products(id, name, image, description, price, created_at)
+VALUES ('c6bbd58e-0551-42fb-a80e-9c5d8beb798e', 'Laptop LG Gram Pro 2024', 'https://product.hstatic.net/1000233206/product/laptop-lg-gram-pro-2024-16z90sp-k-ah75a5_7ffc487808104f6b9548406255db7d5d_grande.png','A nice laptop',48990000,NOW());
+INSERT INTO products(id, name, image, description, price, created_at)
+VALUES ('bf4656f5-aaba-435e-a2c2-69ebce100754', 'Laptop HP 15-fd1039TU 9Z2W7PA', 'https://product.hstatic.net/1000233206/product/laptop-hp-15-fd1039tu-9z2w7pa_c852496efdc849c0bf21dad9cdbfe426_grande.png','A nice laptop',21990000,NOW());
